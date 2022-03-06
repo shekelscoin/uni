4266848056b8a462f7d6e227580217ab96a36611
BT <bt@metaapplebank.com>
2022-03-06 10:11:43 +0000

    second comit uni

---
+++ coinkeys/smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url:  'https://eth-ropsten.alchemyapi.io/v2/<qsJRYUV_FbrOKu7GT8rHaHPn4F8Zubv8Y',
+      accounts: ['68d4b676763128ccd60776bf914f32333dc1652923bf10880571a4c27a20e07e'],
+    },
+  },
+};
